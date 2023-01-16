62d8395e5806226138ff52062b5172a88c377c01
markbateman <markbateman@mac.com>
2023-01-16 09:01:41 +0000

    add dekete other name

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,11 +10,15 @@ if (window.console && window.console.info) {
  // Add new element
  $(".add").click(function(){
 
+  // window.alert("add");
+
+  // govuk-form-group
+
   // Finding total number of elements added
-  var total_element = $(".govuk-form-group").length;
+  var total_element = $(".other-names-govuk-form-group").length;
  
   // last <div> with element class id
-  var lastid = $(".govuk-form-group:last").attr("id");
+  var lastid = $(".other-names-govuk-form-group:last").attr("id");
   var split_id = lastid.split("_");
   var nextindex = Number(split_id[1]) + 1;
 
@@ -22,13 +26,13 @@ if (window.console && window.console.info) {
   // Check total number elements
   if(total_element < max ){
    // Adding new div container after last occurance of element class
-   $(".govuk-form-group:last").after("<div class='govuk-form-group' id='div_"+ nextindex +"'></div>");
+   $(".other-names-govuk-form-group:last").after("<div class='other-names-govuk-form-group' id='div_"+ nextindex +"'></div>");
 
    // Adding label to <div>
    $("#div_" + nextindex).append("<label class='govuk-label' for='official-number_"+ nextindex +"'>Other name "+ nextindex +"</label>");
  
    // Adding element to <div>
-   $("#div_" + nextindex).append("<input class='govuk-input govuk-!-width-one-quarter govuk-!-margin-right-6' autocomplete='off' maxlength='9' type='text' name='official-number_"+ nextindex +"'> <span id='remove_" + nextindex + "' class='govuk-button govuk-button--secondary remove' >Remove "+ nextindex +"</span>");
+   $("#div_" + nextindex).append("<input class='govuk-input govuk-!-width-two-thirds govuk-!-margin-right-6' autocomplete='off' maxlength='9' type='text' name='official-number_"+ nextindex +"'> <span id='remove_" + nextindex + "' class='govuk-button govuk-button--secondary remove' >Remove "+ nextindex +"</span>");
  
   }
  
@@ -37,6 +41,8 @@ if (window.console && window.console.info) {
  // Remove element
  $('.container').on('click','.remove',function(){
  
+  // window.alert("delete");
+
   var id = this.id;
   var split_id = id.split("_");
   var deleteindex = split_id[1];
